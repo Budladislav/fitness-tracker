bcb92dd68ad51851dfd4135872a5d8c988caf0ea
Budladislav <budladislav@gmail.com>
2026-03-30 09:16:34 +0200

    fix(pwa): absolute icon paths and dev mode enabled for PWA v2.4.3

--- vite.config.js
+++ vite.config.js
@@ -24,22 +24,26 @@ export default defineConfig({
         orientation: 'portrait',
         icons: [
           {
-            src: 'icons/android-chrome-192x192.png',
+            src: '/icons/android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'icons/android-chrome-512x512.png',
+            src: '/icons/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'icons/android-chrome-512x512.png',
+            src: '/icons/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
           }
         ]
+      },
+      devOptions: {
+        enabled: true,
+        type: 'module'
       }
     })
   ],
